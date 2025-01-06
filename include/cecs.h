301b8e58be64fd24591835214bc23fa3b75e5a43
matthew.barnard <matthew.barnard@wisk.aero>
2025-01-06 13:24:23 -0700

    better foreach macro

--- include/cecs.h
+++ include/cecs.h
@@ -21,7 +21,7 @@ typedef cecs_id_t cecs_component_id_t;
 extern cecs_component_id_t CECS_NEXT_COMPONENT_ID;
 
 
-#define CECS_ID_OF(type) __cecs_##type##_id
+#define CECS_ID_OF(type)   __cecs_##type##_id
 #define CECS_SIZE_OF(type) __cecs_##type##_size
 
 
@@ -68,44 +68,45 @@ extern cecs_component_id_t CECS_NEXT_COMPONENT_ID;
    &= ~CECS_COMPONENT_TO_BITS(component))
 
 
-#define CECS_COMPONENT_DECL(type)                             \
-  static const size_t CECS_SIZE_OF(type)            = (size_t)0u; \
+#define CECS_COMPONENT_DECL(type)                           \
+  static const size_t CECS_SIZE_OF(type)      = (size_t)0u; \
   static cecs_component_id_t CECS_ID_OF(type) = (cecs_component_id_t)0u
 
 #define CECS_COMPONENT(type) \
   ((__cecs_##type##_id) = (cecs_component_id_t)(CECS_NEXT_COMPONENT_ID++))
 
-#define CONCATENATE(arg1, arg2)   CONCATENATE1(arg1, arg2)
-#define CONCATENATE1(arg1, arg2)  CONCATENATE2(arg1, arg2)
-#define CONCATENATE2(arg1, arg2)  arg1##arg2
 
-#define FOR_EACH_1(what, x, ...) what(x)
-#define FOR_EACH_2(what, x, ...) what(x), FOR_EACH_1(what, __VA_ARGS__)
-#define FOR_EACH_3(what, x, ...) what(x), FOR_EACH_2(what, __VA_ARGS__)
-#define FOR_EACH_4(what, x, ...) what(x), FOR_EACH_3(what, __VA_ARGS__)
-#define FOR_EACH_5(what, x, ...) what(x), FOR_EACH_4(what, __VA_ARGS__)
-#define FOR_EACH_6(what, x, ...) what(x), FOR_EACH_5(what, __VA_ARGS__)
-#define FOR_EACH_7(what, x, ...) what(x), FOR_EACH_6(what, __VA_ARGS__)
-#define FOR_EACH_8(what, x, ...) what(x), FOR_EACH_7(what, __VA_ARGS__)
+#define FE_0(WHAT)
+#define FE_1(WHAT, X) WHAT(X) 
+#define FE_2(WHAT, X, ...) WHAT(X), FE_1(WHAT, __VA_ARGS__)
+#define FE_3(WHAT, X, ...) WHAT(X), FE_2(WHAT, __VA_ARGS__)
+#define FE_4(WHAT, X, ...) WHAT(X), FE_3(WHAT, __VA_ARGS__)
+#define FE_5(WHAT, X, ...) WHAT(X), FE_4(WHAT, __VA_ARGS__)
+//... repeat as needed
+
+#define GET_MACRO(_0,_1,_2,_3,_4,_5,NAME,...) NAME 
+#define FOR_EACH(action,...) \
+  GET_MACRO(_0,__VA_ARGS__,FE_5,FE_4,FE_3,FE_2,FE_1,FE_0)(action,__VA_ARGS__)
 
 #define FOR_EACH_NARG(...) FOR_EACH_NARG_(__VA_ARGS__, FOR_EACH_RSEQ_N())
 #define FOR_EACH_NARG_(...) FOR_EACH_ARG_N(__VA_ARGS__) 
 #define FOR_EACH_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, N, ...) N 
 #define FOR_EACH_RSEQ_N() 8, 7, 6, 5, 4, 3, 2, 1, 0
 
-#define FOR_EACH_(N, what, x, ...) CONCATENATE(FOR_EACH_, N)(what, x, __VA_ARGS__)
-#define FOR_EACH(what, x, ...) FOR_EACH_(FOR_EACH_NARG(x, __VA_ARGS__), what, x, __VA_ARGS__)
 
-#define PRN_STRUCT_OFFSETS_(structure, field) printf(STRINGIZE(structure)":"STRINGIZE(field)" - offset = %d\n", offsetof(structure, field));
-#define PRN_STRUCT_OFFSETS(field) PRN_STRUCT_OFFSETS_(struct a, field)
+/** Get the component of the specified type for the given entity, drawing from
+  * the given query result */
+#define cecs_get(it, entity, type) \
+  (type *)_cecs_get(&it, entity, CECS_ID_OF(type), CECS_SIZE_OF(type))
 
-#define cecs_get(it, entity, type) _cecs_get(&it, entity, CECS_ID_OF(type), CECS_SIZE_OF(type))
-
+/** Create a new entity with the given components */
 #define cecs_create(...) \
   _cecs_create(FOR_EACH_NARG(__VA_ARGS__), FOR_EACH(CECS_ID_OF, __VA_ARGS__))
 
+/** Get an iterator over entities that have the specified components */
 #define cecs_query(...) \
   _cecs_query(FOR_EACH_NARG(__VA_ARGS__), FOR_EACH(CECS_ID_OF, __VA_ARGS__))
+
 
 /** A signature represents the components implemented by a type as a bit set. */
 typedef struct {
