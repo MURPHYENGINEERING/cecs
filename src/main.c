e5ad7331b7b7fd18ef62621534f79a80384103a2
matthew.barnard <matthew.barnard@wisk.aero>
2025-01-08 08:41:54 -0700

    example demonstrating tags

--- src/main.c
+++ src/main.c
@@ -7,19 +7,23 @@
 
 typedef struct {
   float x, y;
-} position_t, velocity_t;
+} has_position_t, has_velocity_t;
 
 
 typedef struct {
-  uint32_t hp;
-} health_t;
+  int32_t hp;
+} has_health_t;
+
+typedef struct {
+} is_alive_t;
 
 
 /* First you have to declare that the components exist. This records vital
 information about their type and assigns each a unique ID. */
-CECS_COMPONENT_DECL(position_t);
-CECS_COMPONENT_DECL(velocity_t);
-CECS_COMPONENT_DECL(health_t);
+CECS_COMPONENT_DECL(has_position_t);
+CECS_COMPONENT_DECL(has_velocity_t);
+CECS_COMPONENT_DECL(has_health_t);
+CECS_COMPONENT_DECL(is_alive_t);
 
 
 
@@ -30,28 +34,39 @@ void move_system()
   cecs_iter_t it;
   cecs_entity_t entity;
 
-  cecs_query(&it, velocity_t);
+  cecs_query(&it, is_alive_t, has_health_t);
+  while ((entity = cecs_iter_next(&it))) {
+    has_health_t *health = cecs_get(entity, has_health_t);
+
+    health->hp -= 10;
+    if (health->hp <= 0) {
+      cecs_remove(entity, is_alive_t);
+    }
+    printf("[alive, health] %llu = %d\n", entity, health->hp);
+  }
+
+  cecs_query(&it, has_velocity_t);
   while ((entity = cecs_iter_next(&it))) {
     printf("[velocity] %llu\n", entity);
   }
 
-  cecs_query(&it, position_t);
+  cecs_query(&it, has_position_t);
   while ((entity = cecs_iter_next(&it))) {
     printf("[position] %llu\n", entity);
   }
 
-  cecs_query(&it, position_t, velocity_t);
+  cecs_query(&it, has_position_t, has_velocity_t);
   while ((entity = cecs_iter_next(&it))) {
     printf("[position, velocity] %llu\n", entity);
   }
   
-  cecs_query(&it, velocity_t, position_t, health_t);
+  cecs_query(&it, has_velocity_t, has_position_t, has_health_t);
   while  ((entity = cecs_iter_next(&it))) {
     /* Getting the component data for a given entity,component pair allows you
     to update it in-situ */
-    position_t *pos = cecs_get(entity, position_t);
-    velocity_t *vel = cecs_get(entity, velocity_t);
-    health_t *health = cecs_get(entity, health_t);
+    has_position_t *pos = cecs_get(entity, has_position_t);
+    has_velocity_t *vel = cecs_get(entity, has_velocity_t);
+    has_health_t *health = cecs_get(entity, has_health_t);
 
     printf("[position, velocity, health] %llu\n", entity);
 
@@ -60,8 +75,6 @@ void move_system()
     /* The "move" system updates position based on velocity. */
     pos->x += vel->x;
     pos->y += vel->y;
-    /* Additionally we mess with health for no good reason */
-    health->hp -= 1u;
   }
 }
 
@@ -69,49 +82,53 @@ void move_system()
 int main()
 {
   printf("Hello, world!\n");
+  printf("================================================================================\n");
 
   /* In addition to declaring the components, they must be registered in some
   setup function. This adds entries to the component table so they can be
   assigned to entities. */
-  CECS_COMPONENT(position_t);
-  CECS_COMPONENT(velocity_t);
-  CECS_COMPONENT(health_t);
+  CECS_COMPONENT(has_position_t);
+  CECS_COMPONENT(has_velocity_t);
+  CECS_COMPONENT(has_health_t);
+  CECS_COMPONENT(is_alive_t);
 
   /* Create dummy data to populate entity's components with */
-  position_t position = { .x = 0.0f, .y = 0.0f };
-  velocity_t velocity = { .x = 1.0f, .y = 1.0f };
-  health_t health = { .hp = 100 };
+  has_position_t position = { .x = 0.0f, .y = 0.0f };
+  has_velocity_t velocity = { .x = 1.0f, .y = 1.0f };
+  has_health_t health = { .hp = 30 };
 
   /* Create an entity implementing a set of compnents */
-  cecs_entity_t entity1 = cecs_create(health_t, position_t);
-  cecs_zero(entity1, position_t, health_t);
+  cecs_entity_t entity1 = cecs_create(is_alive_t, has_health_t, has_position_t);
+  cecs_zero(entity1, has_position_t);
+  cecs_set(entity1, has_health_t, &health);
   
   /* Create an entity and add another component to it */
-  cecs_entity_t entity = cecs_create(velocity_t, position_t);
-  cecs_add(entity, health_t);
+  cecs_entity_t entity = cecs_create(has_velocity_t, has_position_t);
+  cecs_add(entity, has_health_t);
 
   /* Populate component data for the entity with the dummy data from above */
-  cecs_zero(entity, position_t);
-  cecs_set(entity, velocity_t, &velocity);
-  cecs_set(entity, health_t, &health);
+  cecs_zero(entity, has_position_t);
+  cecs_set(entity, has_velocity_t, &velocity);
+  cecs_set(entity, has_health_t, &health);
 
   /* Try adding and removing components to and from the entity after creation */
-  entity = cecs_create(velocity_t);
-  cecs_add(entity, position_t);
-  cecs_remove(entity, position_t);
-  cecs_add(entity, position_t);
-  cecs_add(entity, health_t);
+  entity = cecs_create(has_velocity_t);
+  cecs_add(entity, has_position_t);
+  cecs_remove(entity, has_position_t);
+  cecs_add(entity, has_position_t);
+  cecs_add(entity, has_health_t);
 
-  cecs_zero(entity, position_t);
-  cecs_set(entity, velocity_t, &velocity);
-  cecs_set(entity, health_t, &health);
+  cecs_zero(entity, has_position_t);
+  cecs_set(entity, has_velocity_t, &velocity);
+  cecs_set(entity, has_health_t, &health);
 
-  cecs_remove(entity1, position_t);
+  cecs_remove(entity1, has_position_t);
 
 
   /* Iterate the "move" system, which updates position based on velocity. */
   for (size_t i = 0; i < 4; ++i) {
     move_system();
+    printf("--------------------------------------------------------------------------------\n");
   }
 
   return 0;
