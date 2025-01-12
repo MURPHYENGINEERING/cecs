e3b0f623364c8a1bbe6cb32bfd60e518ae7cccff
Matthew Murphy <matt@murphysys.com>
2025-01-12 12:31:29 -0700

    Add todos for archetype set

--- src/cecs.c
+++ src/cecs.c
@@ -141,6 +141,7 @@ struct archetype {
   /** Number of entities able to be stored in the entities array before resizing */
   size_t cap;
   /** Array of entities implementing this archetype */
+  /* TODO: Replace entity list with a set for faster lookup */
   cecs_entity_t *entities;
 };
 
@@ -598,6 +599,7 @@ static struct component_by_id_table *get_component_by_id(const cecs_component_t 
  * entities implementing that archetype */
 static void add_entity_to_archetype(const cecs_entity_t entity, struct archetype *archetype)
 {
+  /* TODO: Replace archetype entity list with a set */
   /* Don't add duplicates */
   for (size_t i = 0; i < archetype->count; ++i) {
     if (archetype->entities[i] == entity) {
@@ -822,6 +824,7 @@ void _cecs_remove(const cecs_entity_t entity, const cecs_component_t n, ...)
 
   /* Remove the entity from its current archetype */
   struct archetype *archetype = get_archetype_by_sig(sig);
+  /* TOOD: Replace archetype entity list with a set */
   for (cecs_entity_t i = 0; i < archetype->count; ++i) {
     if (archetype->entities[i] == entity) {
       /* Put the last entity in the array into the removed entity's slot */
